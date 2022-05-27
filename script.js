1e632f71bd6557a1fb2418fad0e1af7d547d6e30
JCSkater <jcskater@gmail.com>
2022-05-26 22:23:09 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,15 @@
+// const botao = document.querySelector("div");
+// botao.addEventListener('click', function({
+
+// }));
+
+let meuNome = "João"
+let sobreNome = "Carlos"
+
+//template string
+console.log(`Oá, meu nome é ${meuNome} ${sobreNome}`)
+
+let valor = 1
+
+resultado = valor > 0 ? "sim" : "nao";
+console.log(resultado)
